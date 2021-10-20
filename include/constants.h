256e3bcc9a7982090875c4e1b9c557601dfbaa9f
DUBOSCLARD LÉO <leo.dubosclard@epitech.eu>
2021-10-20 17:57:20 +0200

    jfkd

---
+++ include/constants.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** constants
+** File description:
+** contain constants values
+*/
+
+#ifndef CONSTANTS_H_
+    #define CONSTANTS_H_
+    #define WIDTH   1080
+    #define HEIGHT  720
+
+#endif /* CONSTANTS_H_
